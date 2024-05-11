5db1593d1317b1ad0bf7707dfe4183a2d9230b17
seonyo <jsy6535@naver.com>
2024-05-11 18:25:41 +0900

    ✨ [Feat] (#6) 서버 에러 핸들링 추가

--- src/main/java/com/MtoM/MtoM/global/exception/error/ErrorCode.java
+++ src/main/java/com/MtoM/MtoM/global/exception/error/ErrorCode.java
@@ -1,4 +1,15 @@
 package com.MtoM.MtoM.global.exception.error;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum ErrorCode {
+
+    INTER_SERER_ERROR(500, "SERVER-ERROR-500", "내부 서버 오류가 발생했습니다.");
+
+    private int status;
+    private String errorCode;
+    private String errorMessage;
 }
--- src/main/java/com/MtoM/MtoM/global/exception/error/GlobalExceptionHandler.java
+++ src/main/java/com/MtoM/MtoM/global/exception/error/GlobalExceptionHandler.java
@@ -1,4 +1,18 @@
 package com.MtoM.MtoM.global.exception.error;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex){
+        log.error("handlerException", ex);
+        ErrorResponse response = new ErrorResponse(ErrorCode.INTER_SERER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
