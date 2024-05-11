2e20544b5b548fc8bec861f8c7f494545b4deb28
seonyo <jsy6535@naver.com>
2024-05-11 21:46:31 +0900

    :bug: [Fix] (#6) UserRepository JpaRepository 타입 Long -> String 변경

--- src/main/java/com/MtoM/MtoM/domain/user/repository/UserRepository.java
+++ src/main/java/com/MtoM/MtoM/domain/user/repository/UserRepository.java
@@ -3,6 +3,6 @@ package com.MtoM.MtoM.domain.user.repository;
 import com.MtoM.MtoM.domain.user.domain.UserDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserDomain, Long> {
+public interface UserRepository extends JpaRepository<UserDomain, String> {
 
 }
