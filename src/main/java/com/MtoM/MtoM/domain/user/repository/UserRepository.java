6fa465fef15cd3b738138640185638addb9f5eb2
seonyo <jsy6535@naver.com>
2024-05-11 18:26:02 +0900

    ✨ [Feat] (#6) UserRepository 추가

---
+++ src/main/java/com/MtoM/MtoM/domain/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.MtoM.MtoM.domain.user.repository;
+
+import com.MtoM.MtoM.domain.user.domain.UserDomain;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserDomain, Long> {
+
+}
