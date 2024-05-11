b6c033efa170cb594dbc423903a4decc090bdbd0
seonyo <jsy6535@naver.com>
2024-05-12 01:51:46 +0900

    :bug: [Fix] (#11) ProjectRedisDomain 타입 Integer -> Long으로 변경

--- src/main/java/com/MtoM/MtoM/domain/project/domain/ProjectRedisDomain.java
+++ src/main/java/com/MtoM/MtoM/domain/project/domain/ProjectRedisDomain.java
@@ -2,19 +2,20 @@ package com.MtoM.MtoM.domain.project.domain;
 
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.redis.core.RedisHash;
 
-@Getter
+@Getter @Setter
 @RedisHash(value = "projects")
 public class ProjectRedisDomain {
     @Id
-    private Integer id;
-    private Integer front_member;
-    private Integer current_front_member;
-    private Integer back_member;
-    private Integer currnet_back_member;
-    private Integer disign_member;
-    private Integer current_disign_member;
-    private Integer promoter_member;
-    private Integer current_promoter_member;
+    private Long id;
+    private Long front_member;
+    private Long current_front_member;
+    private Long back_member;
+    private Long currnet_back_member;
+    private Long disign_member;
+    private Long current_disign_member;
+    private Long promoter_member;
+    private Long current_promoter_member;
 }
