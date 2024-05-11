d52c0b6b3297244061aa12fcd4ae56324de757b4
seonyo <jsy6535@naver.com>
2024-05-12 01:02:38 +0900

    ✨ [Feat] (#11) ProjectDomain 생성

---
+++ src/main/java/com/MtoM/MtoM/domain/project/domain/ProjectDomain.java
@@ -0,0 +1,60 @@
+package com.MtoM.MtoM.domain.project.domain;
+
+import com.MtoM.MtoM.domain.user.domain.UserDomain;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "projects")
+public class ProjectDomain {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserDomain user;
+
+    @Column
+    private String img;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column
+    private String recruitment_start;
+
+    @Column
+    private String recruitment_end;
+
+    @Column
+    private String work_start;
+
+    @Column
+    private String work_end;
+
+    @Column
+    private Long frontend_personnel;
+
+    @Column
+    private Long backend_personnel;
+
+    @Column
+    private Long designer_personnel;
+
+    @Column
+    private Long promoter_personnel;
+
+    @Column(columnDefinition = "TEXT")
+    private Long  introduction;
+
+    @Column
+    private Boolean is_matching;
+
+}
