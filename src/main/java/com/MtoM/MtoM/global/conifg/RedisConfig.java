1a8eaba474a1182ff34fc20d7976cc326e457f9e
seonyo <jsy6535@naver.com>
2024-05-12 00:35:39 +0900

    ✨ [Feat] (#11) RedisTemplate 추가

--- src/main/java/com/MtoM/MtoM/global/conifg/RedisConfig.java
+++ src/main/java/com/MtoM/MtoM/global/conifg/RedisConfig.java
@@ -1,10 +1,12 @@
 package com.MtoM.MtoM.global.conifg;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
@@ -19,4 +21,10 @@ public class RedisConfig {
         return new LettuceConnectionFactory(host, port);
     }
 
+    @Bean
+    RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
 }
