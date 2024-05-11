4b5d63ea83ec274f1542d533cfda9d0ba4f75c33
seonyo <jsy6535@naver.com>
2024-05-11 21:55:49 +0900

    ✨ [Feat] (#6) existsByEmail 메소드 추가

--- src/main/java/com/MtoM/MtoM/domain/user/repository/UserRepository.java
+++ src/main/java/com/MtoM/MtoM/domain/user/repository/UserRepository.java
@@ -4,5 +4,6 @@ import com.MtoM.MtoM.domain.user.domain.UserDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserDomain, String> {
+    boolean existsByEmail(String email);
 
 }
