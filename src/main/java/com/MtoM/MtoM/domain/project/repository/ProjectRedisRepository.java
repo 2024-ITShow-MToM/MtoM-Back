12e5e49f1408ab517109010878fac6b4b61efaf3
SunYoung Jung <88700744+seonyo@users.noreply.github.com>
2024-05-15 01:44:17 +0900

    Revert "merge"

--- src/main/java/com/MtoM/MtoM/domain/project/repository/ProjectRedisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.MtoM.MtoM.domain.project.repository;
-
-import com.MtoM.MtoM.domain.project.domain.ProjectRedisDomain;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProjectRedisRepository extends CrudRepository<ProjectRedisDomain, Integer> {
-
-}
