12cd5462005a099007bdaa2270ae292eb6f8c665
seonyo <jsy6535@naver.com>
2024-05-16 00:43:55 +0900

    ✨ Feat (#20) EmailNotFound 에러 핸들링 추가

--- src/main/java/com/MtoM/MtoM/global/exception/error/GlobalExceptionHandler.java
+++ src/main/java/com/MtoM/MtoM/global/exception/error/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.MtoM.MtoM.global.exception.error;
 
 import com.MtoM.MtoM.global.exception.EmailDuplicateException;
+import com.MtoM.MtoM.global.exception.EmailNotFoundException;
 import com.MtoM.MtoM.global.exception.IdDuplicateException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -28,6 +30,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EmailDuplicateException.class)
     public ResponseEntity<ErrorResponse> emailDuplicateException(EmailDuplicateException ex){
         log.error("emailDuplicateException", ex);
+        ErrorResponse response = new ErrorResponse(ex.getErrorCode());
+        return new ResponseEntity<>(response, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
+    }
+
+    @ExceptionHandler(EmailNotFoundException.class)
+    public ResponseEntity<ErrorResponse> emailNotFoundException(EmailNotFoundException ex){
+        log.error("emailNotFoundException", ex);
         ErrorResponse response = new ErrorResponse(ex.getErrorCode());
         return new ResponseEntity<>(response, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
     }
