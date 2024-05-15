b31ec02b01e6a0b35dce3bce38b25ccbf22dc92e
seonyo <jsy6535@naver.com>
2024-05-16 04:43:46 +0900

    ✨ Feat (#20) 프로필 업로드하는 컨트롤러 구현

--- src/main/java/com/MtoM/MtoM/domain/user/controller/UserController.java
+++ src/main/java/com/MtoM/MtoM/domain/user/controller/UserController.java
@@ -3,16 +3,21 @@ package com.MtoM.MtoM.domain.user.controller;
 import com.MtoM.MtoM.domain.user.dto.RegisterProfileInfoDto;
 import com.MtoM.MtoM.domain.user.dto.RegisterRequestDto;
 import com.MtoM.MtoM.domain.user.service.UserService;
+import com.MtoM.MtoM.global.S3Service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
+    private final S3Service s3UploadService;
 
     @PostMapping
     public ResponseEntity<String> registerUser(@RequestBody RegisterRequestDto requestDto){
@@ -24,5 +29,17 @@ public class UserController {
     public ResponseEntity<String> registerProfileInfo(@RequestBody RegisterProfileInfoDto requestDto){
         userService.registerProfileInfo(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body("프로필 정보 등록 성공했습니다.");
+    }
+
+    @PostMapping("/profile/img")
+    public ResponseEntity<String> registerProfileImg (@RequestParam(value = "profile") MultipartFile file,
+                                                      @RequestParam(value = "id") String id) {
+        try{
+            s3UploadService.uploadProfileFile(id, file, "profile");
+            return ResponseEntity.ok().body("프로필 이미지 업로드에 성공했습니다");
+        }catch (IOException e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("프로필 이미지 업로드에 실패했습니다");
+        }
+
     }
 }
