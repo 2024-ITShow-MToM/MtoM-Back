934a62635184b9b149b3929269090d2761aa0381
seonyo <jsy6535@naver.com>
2024-06-17 18:42:16 +0900

    ✨ [Feat] 추천 멘토 불러오는 api 구현

--- src/main/java/com/MtoM/MtoM/domain/mentor/dto/req/MentorRecommendRequest.java
+++ src/main/java/com/MtoM/MtoM/domain/mentor/dto/req/MentorRecommendRequest.java
@@ -1,2 +1,13 @@
-package com.MtoM.MtoM.domain.mentor.dto.req;public class MentorRecommendRequest {
-}
+    package com.MtoM.MtoM.domain.mentor.dto.req;
+
+    import com.MtoM.MtoM.domain.mentor.dto.Worry;
+    import com.MtoM.MtoM.domain.user.domain.Major;
+    import lombok.Getter;
+
+    @Getter
+    public class MentorRecommendRequest {
+        private String userId;
+        private Worry worry;
+        private Major major;
+        private String personal;
+    }
--- src/main/java/com/MtoM/MtoM/domain/mentor/dto/res/MentorRecommendResponse.java
+++ src/main/java/com/MtoM/MtoM/domain/mentor/dto/res/MentorRecommendResponse.java
@@ -1,2 +1,24 @@
-package com.MtoM.MtoM.domain.mentor.dto.res;public class MentorRecommendResponse {
+package com.MtoM.MtoM.domain.mentor.dto.res;
+
+import com.MtoM.MtoM.domain.user.domain.Major;
+import com.MtoM.MtoM.domain.user.domain.UserDomain;
+import lombok.Getter;
+
+@Getter
+public class MentorRecommendResponse {
+    private String userId;
+    private String name;
+    private Major major;
+    private String hashtag;
+    private String introduction;
+    private String profile;
+
+    public MentorRecommendResponse(UserDomain user){
+        this.userId = user.getId();
+        this.name = user.getName();
+        this.major = user.getMajor();
+        this.hashtag = user.getMentoring_topics();
+        this.introduction = user.getIntroduction();
+        this.profile = user.getProfile();
+    }
 }
