714bfc1f4be555136ff64aa37894e041fb94fdf6
seonyo <jsy6535@naver.com>
2024-06-19 16:16:51 +0900

    ✨ [Feat] 메세지 보내는 컨트롤러 구현

--- src/main/java/com/MtoM/MtoM/domain/groupChat/GroupChatController.java
+++ src/main/java/com/MtoM/MtoM/domain/groupChat/GroupChatController.java
@@ -1,15 +1,17 @@
 package com.MtoM.MtoM.domain.groupChat;
 
+import com.MtoM.MtoM.domain.groupChat.domain.GroupChatContentDomain;
+import com.MtoM.MtoM.domain.groupChat.dto.req.SendChatRequestDto;
 import com.MtoM.MtoM.domain.groupChat.dto.res.GroupChatListResponseDto;
-import com.MtoM.MtoM.domain.groupChat.service.GroupChartList;
+import com.MtoM.MtoM.domain.groupChat.repository.GroupChatContentRepository;
+import com.MtoM.MtoM.domain.groupChat.service.GroupChartListService;
+import com.MtoM.MtoM.domain.groupChat.service.SendGroupChatService;
 import com.MtoM.MtoM.global.message.ResponsePayload;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.query.sql.internal.ParameterRecognizerImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,12 +19,20 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/chats/groups")
 public class GroupChatController {
-    private final GroupChartList groupChartList;
+    private final GroupChartListService groupChartList;
+    private final SendGroupChatService sendGroupChatService;
 
     //나의 채팅 내역 불러오기
     @GetMapping("/list/{userId}")
     public ResponseEntity<ResponsePayload<List<GroupChatListResponseDto>>> groupChatList(@PathVariable("userId") String userId){
         List<GroupChatListResponseDto> chats = groupChartList.execute(userId);
         return new ResponseEntity<>(new ResponsePayload<>("chat list successfulllyl", chats), HttpStatus.OK);
+    }
+
+    //채팅 보내기
+    @PostMapping("/{userId}")
+    public ResponseEntity<ResponsePayload<GroupChatContentDomain>> sendChat(@RequestBody SendChatRequestDto requestDto){
+        GroupChatContentDomain content = sendGroupChatService.execute(requestDto);
+        return new ResponseEntity<>(new ResponsePayload<>("chat send successfully", content), HttpStatus.CREATED);
     }
 }
