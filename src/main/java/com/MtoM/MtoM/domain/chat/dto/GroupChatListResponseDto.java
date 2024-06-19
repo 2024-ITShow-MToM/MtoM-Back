1de10c7bdbef46098b0388b091b4e34436fce4df
seonyo <jsy6535@naver.com>
2024-06-19 15:11:30 +0900

    ✨ [Feat] GroupChatListResponseDto 생성

---
+++ src/main/java/com/MtoM/MtoM/domain/chat/dto/GroupChatListResponseDto.java
@@ -0,0 +1,24 @@
+package com.MtoM.MtoM.domain.chat.dto;
+
+import com.MtoM.MtoM.domain.groupChat.domain.GroupChatDomain;
+import com.MtoM.MtoM.domain.groupChat.service.GroupChartList;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.query.sql.internal.ParameterRecognizerImpl;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalTime;
+
+@Getter @Setter
+public class GroupChatListResponseDto {
+    private Long projectId;
+    private String name;
+    private String profile;
+
+
+    public GroupChatListResponseDto(GroupChatDomain chats){
+        this.projectId = chats.getProject().getId();
+        this.name = chats.getProject().getUser().getName();
+        this.profile = chats.getProject().getImg();
+    }
+}
