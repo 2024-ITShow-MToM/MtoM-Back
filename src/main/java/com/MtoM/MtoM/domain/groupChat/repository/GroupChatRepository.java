1cb5c4e06f002bedf4b9b7b14bfb4f9fc928cb86
seonyo <jsy6535@naver.com>
2024-06-19 15:11:40 +0900

    ✨ [Feat] findAllByProjectIdIn 메소드 추가

--- src/main/java/com/MtoM/MtoM/domain/groupChat/repository/GroupChatRepository.java
+++ src/main/java/com/MtoM/MtoM/domain/groupChat/repository/GroupChatRepository.java
@@ -1,8 +1,13 @@
 package com.MtoM.MtoM.domain.groupChat.repository;
 
 import com.MtoM.MtoM.domain.groupChat.domain.GroupChatDomain;
+import com.MtoM.MtoM.domain.groupChat.service.GroupChartList;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface GroupChatRepository extends JpaRepository<GroupChatDomain, Long> {
     boolean existsByProjectId(Long projectId);
+
+    List<GroupChatDomain> findAllByProjectIdIn(List<Long> projectIds);
 }
